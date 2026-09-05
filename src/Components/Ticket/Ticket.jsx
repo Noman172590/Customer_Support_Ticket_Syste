7ff350239c4141bb112acef2a157833fc07e6d9c
Noman172590 <nomanjahan866615@gmail.com>
2026-09-05 22:57:43 +0600

    update code ticket

--- src/Components/Ticket/Ticket.jsx
+++ src/Components/Ticket/Ticket.jsx
@@ -1,19 +1,46 @@
-import React, { use } from 'react';
+import React, { use } from "react";
+import { DateIcon } from "../../assets";
 
-const Ticket = ({data}) => {
-   const overAllData=use(data)
-   const singeData=overAllData.data
-   console.log(singeData);
-    return (
-        <div className='max-w-7xl mx-auto border py-20'>
-            <dinov className='grid grid-cols-[minmax(0,1fr)_358px]'>
-                <div className='border'>
-                    Customer Tickets
+const Ticket = ({ data }) => {
+  const overAllData = use(data);
+  const singeData = overAllData.data;
+  console.log(singeData);
+  return (
+    <div className="max-w-7xl mx-auto border py-20">
+      <div className="grid grid-cols-[minmax(0,1fr)_358px]">
+        <div className="border">
+          <p>Customer Tickets</p>
+          <div className="grid grid-cols-2 gap-6">
+            {singeData.map((item) => (
+              <div className="border p-2">
+                <div className="flex justify-between">
+                  <p className="text-[#001931] font-bold">
+  {item.title}
+</p>
+                  <p>{item.status}</p>
                 </div>
-                <div className='border'>Status</div>
-            </dinov>
+                <p>{item.description}</p>
+                <div className="flex justify-between">
+                  <div className="flex gap-2">
+                    <p>#{item.id}</p>
+                    <p>{item.priority} PRIORITY</p>
+                  </div>
+                  <div className="flex gap-2">
+                    <p>{item.customer}</p>
+                    <div className="flex gap-2 ">
+                      <DateIcon />
+                      {item.date}
+                    </div>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
         </div>
-    );
+        <div className="border">Status</div>
+      </div>
+    </div>
+  );
 };
 
 export default Ticket;
--- src/assets/index.js
+++ src/assets/index.js
@@ -1,5 +1,10 @@
 // image part 
 import backgroundImage from './image/vector1.png';
+import DateIcon from './image/ri_calendar-line.svg?react';
 
-
+// export in image 
 export{backgroundImage}
+
+//export in svg
+
+export{DateIcon}
